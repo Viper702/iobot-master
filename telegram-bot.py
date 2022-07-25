df4d95f5d039bb43d5c6712fd0fff6815413ef2a
Viper <viper@demi>
2022-07-24 19:07:17 -0700

    Trimming new code.

--- telegram-bot.py
+++ telegram-bot.py
@@ -5,22 +5,21 @@ import json, os, string, sys, threading, logging, time, re, random
 import openai
 import value_set
 
-#OpenAI API key
-aienv = os.getenv('OPENAI_KEY')
+# OpenAI API key
+aienv = os.getenv("OPENAI_KEY")
 if aienv == None:
     openai.api_key = value_set.openai_key
 else:
     openai.api_key = aienv
 print(aienv)
 
-#Telegram bot key
-tgenv = os.getenv('TELEGRAM_KEY')
+# Telegram bot key
+tgenv = os.getenv("TELEGRAM_KEY")
 if tgenv == None:
- tgkey = value_set.tgkey
+    tgkey = value_set.tgkey
 else:
     tgkey = tgenv
 print(tgenv)
-
 
 
 # Lots of console output
@@ -30,14 +29,14 @@ debug = True
 timstart = 300
 tim = 1
 
-#Defaults
+# Defaults
 user = ""
 running = False
 cache = None
 qcache = None
 chat_log = None
-#botname = ''
-#username = ''
+# botname = ''
+# username = ''
 botname = value_set.botname
 username = value_set.username
 
@@ -46,8 +45,9 @@ max = int(3000)
 
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
 
@@ -55,7 +55,7 @@ completion = openai.Completion()
 
 
 ##################
-#Command handlers#
+# Command handlers#
 ##################
 def start(bot, update):
     """Send a message when the command /start is issued."""
@@ -70,21 +70,27 @@ def start(bot, update):
         chat_log = None
         cache = None
         qcache = None
-#        botname = ''
-#        username = ''
+        #        botname = ''
+        #        username = ''
         botname = value_set.botname
         username = value_set.username
 
-        update.message.reply_text('Hi, I\'ll be with you shortly.')
-        return 
+        update.message.reply_text("Hi, I'll be with you shortly.")
+        return
     else:
-        update.message.reply_text('I\'m in another conversation but will get back to you in ' + left + ' seconds?')
+        update.message.reply_text(
+            "I'm in another conversation but will get back to you in "
+            + left
+            + " seconds?"
+        )
         return
 
 
 def help(bot, update):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('[/reset] resets the conversation,\n [/retry] retries the last output,\n [/username name] sets your name to the bot, default is "Human",\n [/botname name] sets the bots character name, default is "AI"')
+    update.message.reply_text(
+        '[/reset] resets the conversation,\n [/retry] retries the last output,\n [/username name] sets your name to the bot, default is "Human",\n [/botname name] sets the bots character name, default is "AI"'
+    )
 
 
 def reset(bot, update):
@@ -100,24 +106,26 @@ def reset(bot, update):
         chat_log = None
         cache = None
         qcache = None
-#        botname = ''
-#        username = ''
+        #        botname = ''
+        #        username = ''
         botname = value_set.botname
         username = value_set.username
-        update.message.reply_text('Bot has been reset, send a message!')
+        update.message.reply_text("Bot has been reset, send a message!")
         return
     if tim == 1:
         chat_log = None
         cache = None
         qcache = None
-#        botname = ''
-#        username = ''
+        #        botname = ''
+        #        username = ''
         botname = value_set.botname
         username = value_set.username
-        update.message.reply_text('Bot has been reset, send a message!')
-        return 
+        update.message.reply_text("Bot has been reset, send a message!")
+        return
     else:
-        update.message.reply_text('I\'m in another conversation and wil be with you in ' + left + ' seconds?')
+        update.message.reply_text(
+            "I'm in another conversation and wil be with you in " + left + " seconds?"
+        )
         return
 
 
@@ -132,23 +140,35 @@ def retry(bot, update):
     left = str(tim)
     if user == update.message.from_user.id:
         new = True
-        comput = threading.Thread(target=wait, args=(bot, update, botname, username, new,))
+        comput = threading.Thread(
+            target=wait,
+            args=(
+                bot,
+                update,
+                botname,
+                username,
+                new,
+            ),
+        )
         comput.start()
         return
     if tim == 1:
         chat_log = None
         cache = None
         qcache = None
-#        botname = ''
-#        username = ''
+        #        botname = ''
+        #        username = ''
         botname = value_set.botname
         username = value_set.username
 
-        update.message.reply_text('Send a message!')
-        return 
-    else:
-        update.message.reply_text('I\'m in another conversation and wil be with you in ' + left + ' seconds?')
+        update.message.reply_text("Send a message!")
         return
+    else:
+        update.message.reply_text(
+            "I'm in another conversation and wil be with you in " + left + " seconds?"
+        )
+        return
+
 
 def runn(bot, update):
     """Send a message when a message is received."""
@@ -158,7 +178,7 @@ def runn(bot, update):
     if "/botname " in update.message.text:
         try:
             string = update.message.text
-            charout = string.split("/botname ",1)[1]
+            charout = string.split("/botname ", 1)[1]
             botname = charout
             response = "The bot character name set to: " + botname
             update.message.reply_text(response)
@@ -168,7 +188,7 @@ def runn(bot, update):
     if "/username " in update.message.text:
         try:
             string = update.message.text
-            userout = string.split("/username ",1)[1]
+            userout = string.split("/username ", 1)[1]
             username = userout
             response = "Your character name set to: " + username
             update.message.reply_text(response)
@@ -176,7 +196,16 @@ def runn(bot, update):
             update.message.reply_text(e)
         return
     else:
-        comput = threading.Thread(target=interact, args=(bot, update, botname, username, new,))
+        comput = threading.Thread(
+            target=interact,
+            args=(
+                bot,
+                update,
+                botname,
+                username,
+                new,
+            ),
+        )
         comput.start()
 
 
@@ -191,7 +220,16 @@ def wait(bot, update, botname, username, new):
         user = update.message.from_user.id
     if user == update.message.from_user.id:
         tim = timstart
-        compute = threading.Thread(target=interact, args=(bot, update, botname, username, new,))
+        compute = threading.Thread(
+            target=interact,
+            args=(
+                bot,
+                update,
+                botname,
+                username,
+                new,
+            ),
+        )
         compute.start()
         if running == False:
             while tim > 1:
@@ -203,33 +241,38 @@ def wait(bot, update, botname, username, new):
                 cache = None
                 qcache = None
                 user = ""
-#                username = ''
-#                botname = ''
+                #                username = ''
+                #                botname = ''
                 username = value_set.username
                 botname = value_set.botname
 
-                update.message.reply_text('Timer has run down, bot has been reset to defaults.')
+                update.message.reply_text(
+                    "Timer has run down, bot has been reset to defaults."
+                )
                 running = False
     else:
         left = str(tim)
-        update.message.reply_text('I\'m in another conversation and will be with you in ' + left + ' seconds?')
+        update.message.reply_text(
+            "I'm in another conversation and will be with you in " + left + " seconds?"
+        )
 
 
 ################
-#Main functions#
+# Main functions#
 ################
 def limit(text, max):
-    if (len(text) >= max):
+    if len(text) >= max:
         inv = max * -1
         print("Reducing length of chat history... This can be a bit buggy.")
         nl = text[inv:]
-        text = re.search(r'(?<=\n)[\s\S]*', nl).group(0)
+        text = re.search(r"(?<=\n)[\s\S]*", nl).group(0)
         return text
     else:
         return text
 
+
 ################ define ask
-#def ask(username, botname, question, chat_log=None):
+# def ask(username, botname, question, chat_log=None):
 #    if chat_log is None:
 #        chat_log = 'The following is a chat between two users:\n\n'
 #    now = datetime.now()
@@ -253,27 +296,33 @@ def ask(username, botname, question, chat_log=None):
         chat_log = value_set.hiddenprompt
     now = datetime.now()
     ampm = now.strftime("%I:%M %p")
-    t = '[' + ampm + '] '
-    prompt = f'{chat_log}{t}{username}: {question}\n{t}{botname}:'
+    t = "[" + ampm + "] "
+    prompt = f"{chat_log}{t}{username}: {question}\n{t}{botname}:"
     response = completion.create(
-        prompt=prompt, engine=value_set.engine, stop=value_set.stop, temperature=value_set.temperature,
-        top_p=value_set.top_p, frequency_penalty=value_set.frequency_penalty, presence_penalty=value_set.presence_penalty, 
+        prompt=prompt,
+        engine=value_set.engine,
+        stop=value_set.stop,
+        temperature=value_set.temperature,
+        top_p=value_set.top_p,
+        frequency_penalty=value_set.frequency_penalty,
+        presence_penalty=value_set.presence_penalty,
         best_of=value_set.best_of,
-        max_tokens=value_set.max_tokens)
+        max_tokens=value_set.max_tokens,
+    )
     answer = response.choices[0].text.strip()
     return answer
     # fp = 15 pp= 1 top_p = 1 temp = 0.9
 
 
-
 def append_interaction_to_chat_log(username, botname, question, answer, chat_log=None):
     if chat_log is None:
-        chat_log = 'The following is a chat between two users:\n\n'
+        chat_log = "The following is a chat between two users:\n\n"
     chat_log = limit(chat_log, max)
     now = datetime.now()
     ampm = now.strftime("%I:%M %p")
-    t = '[' + ampm + '] '
-    return f'{chat_log}{t}{username}: {question}\n{t}{botname}: {answer}\n'
+    t = "[" + ampm + "] "
+    return f"{chat_log}{t}{username}: {question}\n{t}{botname}: {answer}\n"
+
 
 def interact(bot, update, botname, username, new):
     global chat_log
@@ -288,8 +337,10 @@ def interact(bot, update, botname, username, new):
         if debug == True:
             print("Sentiment of input:\n")
             print(vs)
-        if vs['neg'] > 1:
-            update.message.reply_text('Hey look this is all going way south here! Lets talk about something else or now.')
+        if vs["neg"] > 1:
+            update.message.reply_text(
+                "Hey look this is all going way south here! Lets talk about something else or now."
+            )
             return
     if new == True:
         if debug == True:
@@ -303,34 +354,38 @@ def interact(bot, update, botname, username, new):
         question = text
         qcache = question
         cache = chat_log
-    #update.message.reply_text('Computing...')
+    # update.message.reply_text('Computing...')
     try:
         answer = ask(username, botname, question, chat_log)
         if debug == True:
             print("Input:\n" + question)
             print("Output:\n" + answer)
             print("====================")
-        stripes = answer.encode(encoding=sys.stdout.encoding,errors='ignore')
+        stripes = answer.encode(encoding=sys.stdout.encoding, errors="ignore")
         decoded = stripes.decode("utf-8")
         out = str(decoded)
         vs = analyzer.polarity_scores(out)
         if debug == True:
             print("Sentiment of output:\n")
             print(vs)
-        if vs['neg'] > 1:
-            update.message.reply_text('I do not think I could provide you a good answer for this. Use /retry to get positive output.')
+        if vs["neg"] > 1:
+            update.message.reply_text(
+                "I do not think I could provide you a good answer for this. Use /retry to get positive output."
+            )
             return
         update.message.reply_text(out)
-        chat_log = append_interaction_to_chat_log(username, botname, question, answer, chat_log)
+        chat_log = append_interaction_to_chat_log(
+            username, botname, question, answer, chat_log
+        )
         if debug == True:
             #### Print the chat log for debugging
-            print('-----PRINTING CHAT LOG-----')
+            print("-----PRINTING CHAT LOG-----")
             print(chat_log)
-            print('-----END CHAT LOG-----')
+            print("-----END CHAT LOG-----")
     except Exception as e:
-            print(e)
-            errstr = str(e)
-            update.message.reply_text(errstr)
+        print(e)
+        errstr = str(e)
+        update.message.reply_text(errstr)
 
 
 def error(bot, update):
@@ -355,9 +410,9 @@ def main():
     dp.add_error_handler(error)
     # Start the Bot
     updater.start_polling()
-   
+
     updater.idle()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
